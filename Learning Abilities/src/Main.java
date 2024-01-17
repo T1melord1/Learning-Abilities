e2aed6007d1bd972761703a45991f998616e4659
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-17 16:50:23 +0300

    Добавлен объект для Scanner

---
+++ Learning Abilities/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(5 > 3);
+        int a = new Scanner(System.in).nextInt();
+        System.out.println(a);
+    }
+}
